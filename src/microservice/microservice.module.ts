7884b7b9bb194ba97807672bc4027afef25bd1cc
fradotech <fradotech.id@gmail.com>
2023-11-22 13:46:19 +0700

    feat: wrap product option variable

--- src/microservice/microservice.module.ts
+++ src/microservice/microservice.module.ts
@@ -1,22 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { join } from 'path';
+import { ClientsModule } from '@nestjs/microservices';
+import { PRODUCT_OPTION } from './product/product.option';
 import { ProductService } from './product/product.service';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'PRODUCT_PACKAGE',
-        transport: Transport.GRPC,
-        options: {
-          url: '127.0.0.1:5001',
-          package: 'product',
-          protoPath: join(__dirname, 'product/product.proto'),
-        },
-      },
-    ]),
-  ],
+  imports: [ClientsModule.register([PRODUCT_OPTION])],
   controllers: [],
   providers: [ClientsModule, ProductService],
   exports: [ClientsModule, ProductService],
